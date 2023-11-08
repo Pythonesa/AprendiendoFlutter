4ac4f52f0f9eac9195713466074787350c1f3501
Pythonesa <pythonesamar@gmail.com>
2023-11-08 19:10:41 -0300

    init widgets app

---
+++ widgets_app/lib/config/theme/app_theme.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+const colorList = <Color>[
+  Colors.red,
+  Colors.pink,
+  Colors.purple,
+  Colors.deepPurple,
+  Colors.indigo,
+  Colors.blue,
+  Colors.lightBlue,
+  Colors.cyan,
+  Colors.teal,
+  Colors.green,
+  Colors.lightGreen,
+  Colors.lime,
+  Colors.yellow,
+  Colors.amber,
+  Colors.orange,
+  Colors.deepOrange,
+  Colors.brown,
+  Colors.grey,
+  Colors.blueGrey,
+];
+
+class AppTheme {
+  final int selectedColor;
+
+  AppTheme({this.selectedColor = 0})
+      : assert(selectedColor >= 0, 'Selected color must be greater than 0'),
+        assert(selectedColor < colorList.length,
+            'Selected color must be less than ${colorList.length - 1}');
+
+  ThemeData getTheme() => ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: colorList[selectedColor],
+      );
+}
---
+++ widgets_app/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:widgets_app/config/theme/app_theme.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme(selectedColor: 2).getTheme(),
+      home: Scaffold(
+        body: Center(
+          child: FilledButton(child: const Text('Holiwis'), onPressed: () {}),
+        ),
+      ),
+    );
+  }
+}
